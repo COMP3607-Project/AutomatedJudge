059598ce66ea73248a2602a523c3ddf6f4d26d57
100levelz <j100levelz@gmail.com>
2023-11-13 13:00:40 -0400

    added code to create a empty test pdf

--- src/main/java/comp3607project/App.java
+++ src/main/java/comp3607project/App.java
@@ -1,13 +1,26 @@
 package comp3607project;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Document document = new Document();
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+            document.open();
+            document.add(new Paragraph("Hello World!"));
+            document.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
