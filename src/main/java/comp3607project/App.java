11f9524e238e4df25a5bca31cebfdfe972a2c64b
khadishaclarke <khadishaclarke.n@gmail.com>
2023-11-14 12:47:49 -0400

    added table format to pdf

--- src/main/java/comp3607project/App.java
+++ src/main/java/comp3607project/App.java
@@ -3,7 +3,9 @@ package comp3607project;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
-
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.Phrase;
 import java.io.FileOutputStream;
 /**
  * Hello world!
@@ -16,7 +18,35 @@ public class App
         try {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
             document.open();
-            document.add(new Paragraph("Hello World!"));
+
+            // Create a table with 4 columns
+            PdfPTable table = new PdfPTable(4);
+            table.setWidthPercentage(100); // Full Width
+            table.setSpacingBefore(10f); // Space before table
+            table.setSpacingAfter(10f); // Space after table
+
+            // Add Column headers
+            PdfPTable headerTable = new PdfPTable(4);
+            headerTable.setWidthPercentage(100); // Full Width
+            headerTable.setSpacingBefore(10f); // Space before table
+
+            headerTable.addCell(new PdfPCell(new Phrase("Class Name")));
+            headerTable.addCell(new PdfPCell(new Phrase("Test")));
+            headerTable.addCell(new PdfPCell(new Phrase("Mark")));
+            headerTable.addCell(new PdfPCell(new Phrase("Response")));
+
+            document.add(headerTable);
+
+            // Add placeholder data
+            for (int i = 0; i < 5; i++) {
+                table.addCell("Class Name " + i);
+                table.addCell("Test " + i);
+                table.addCell("Mark " + i);
+                table.addCell("Response " + i);
+            }
+
+            document.add(table);
+
             document.close();
             writer.close();
         } catch (Exception e) {
