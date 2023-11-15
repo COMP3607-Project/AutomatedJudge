561b72513b00c4b20d6700aa32f8eba86e98f637
khadishaclarke <khadishaclarke.n@gmail.com>
2023-11-15 15:47:45 -0400

    report header added

---
+++ src/main/java/comp3607project/ReportHeader.java
@@ -0,0 +1,49 @@
+package comp3607project;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+
+public class ReportHeader {
+
+    private Font bold;
+
+    public ReportHeader(){
+        this.bold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+    }
+
+    public void addHeader(Document document) {
+        try {
+            addLogo(document);
+            addUniversityName(document);
+            addStudentInfo(document);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addLogo(Document document) throws Exception{
+        Image image = Image.getInstance("https://i0.wp.com/www.caribbeanclimate.bz/wp-content/uploads/2018/01/UWI-logo.jpg?ssl=1");
+        image.scaleAbsolute(50f, 50f); 
+        image.setAlignment(Image.ALIGN_CENTER); 
+        document.add(image);
+    }
+
+    private void addUniversityName(Document document) throws DocumentException {
+        Paragraph universityName = new Paragraph("The University of the West Indies, St. Augustine", bold);
+        universityName.setAlignment(Paragraph.ALIGN_CENTER); 
+        document.add(universityName);
+    }
+
+    public void addStudentInfo(Document document){
+        Paragraph studentInfo = new Paragraph("Student ID: " /*+ studentId*/ + "\nAssignment Number: " /*+ assignmentNumber*/ +"\nFeedback Report", bold);
+        studentInfo.setAlignment(Paragraph.ALIGN_CENTER); 
+        try {
+            document.add(studentInfo);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }   
+}
