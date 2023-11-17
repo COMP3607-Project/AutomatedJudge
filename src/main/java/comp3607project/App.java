a548370842182b4339919bf159f37b83acf3a2a9
100levelz <j100levelz@gmail.com>
2023-11-17 10:21:15 -0400

    removed PdfDocument interface

--- src/main/java/comp3607project/App.java
+++ src/main/java/comp3607project/App.java
@@ -3,16 +3,16 @@ package comp3607project;
 public class App {
     public static void main(String[] args) {
 
-        ReportContent report = new ReportContent();
-        FeedbackReport r = new FeedbackReport(report);
+        ReportContent content = new ReportContent();
 
-        report.addFeedback(new Feedback("Flight", "Test 1", 10, "Nice!"));
-        report.addFeedback(new Feedback("Flight", "Test 2", 10, "Good job!"));
-        report.addFeedback(new Feedback("Passenger", "Test 3", 10, "Well done!"));
-        report.addFeedback(new Feedback("Passenger", "Test 4", 10, "Excellent!"));
-        report.addFeedback(new Feedback("Luggage", "Test 5", 10, "Great!"));
-        report.addFeedback(new Feedback("Luggage", "Test 6", 10, "Awesome!"));
+        content.addFeedback(new Feedback("Flight", "Test 1", 10, "Nice!"));
+        content.addFeedback(new Feedback("Flight", "Test 2", 10, "Good job!"));
+        content.addFeedback(new Feedback("Passenger", "Test 3", 10, "Well done!"));
+        content.addFeedback(new Feedback("Passenger", "Test 4", 10, "Excellent!"));
+        content.addFeedback(new Feedback("Luggage", "Test 5", 10, "Great!"));
+        content.addFeedback(new Feedback("Luggage", "Test 6", 10, "Awesome!"));
         
-        r.generateDocument();
+        FeedbackReport report = new FeedbackReport(content);
+        report.generateDocument();
     }
 }
