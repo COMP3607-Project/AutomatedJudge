8f399828f4ee4882fc87b710711c27a4c9f7ba6f
vanessa <98173531+vnsa-abn@users.noreply.github.com>
2023-11-18 02:11:26 +0000

    implement pattern

--- src/main/java/comp3607project/App.java
+++ src/main/java/comp3607project/App.java
@@ -1,13 +1,33 @@
-package comp3607project;
+import java.io.File;
+import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        //runner class to ensure that everything works
+        Folder folderTest;
+        String zipFilePath = "C:\\Users\\vanes\\OneDrive - The University of the West Indies, St. Augustine\\Documents\\Year 3\\COMP 3607\\CWE1 Sample papers.zip";
+        String destDirectory = "C:\\Users\\vanes\\OneDrive - The University of the West Indies, St. Augustine\\Documents\\Year 3\\COMP 3607\\Lab5";
+
+        folderTest = new Folder(zipFilePath, destDirectory);
+
+        File unzippedFolder = new File(destDirectory);
+        String contents[] = unzippedFolder.list();
+        System.out.println("List of files and directories in directory:");
+
+        for(int i=0; i<contents.length; i++) {
+            System.out.println(contents[i]);
+        }
+
+        // Use iterator pattern to process each student submission folder
+        Iterator itr = new Folder(zipFilePath, destDirectory).createIterator();
+        
+        while (itr.hasNext()) {
+            //open each student's submission folder
+            //and copy java files to test files folder 
+
+            //ie. Folder test = new Folder(itr.next, testFilesFolder);
+        } 
     }
 }
--- src/main/java/comp3607project/Folder.java
+++ src/main/java/comp3607project/Folder.java
@@ -1,12 +1,10 @@
-package comp3607project;
-
 import java.io.File;
 import java.io.IOException;
- 
-public class Folder{
+
+public class Folder implements Container{
     private File unzipFilePath;
     private UnzipUtility uz = new UnzipUtility();
-    private File[] contents;
+    private File[] foldercontents;
 
     public Folder(String zipFilePath, String destDirectory) throws IOException{ 
         uz.unzip(zipFilePath, destDirectory);
@@ -17,11 +15,40 @@ public class Folder{
         unzipFilePath = uz.getDestDir();
     }
 
-    public File getFilePath() {
+    public File getUZFilePath() {
         return unzipFilePath;
     }
 
-    //use iterator design pattern to get all contents of a folder
-    //use unzipFilePath to get directories and store using their file paths
-}
+    // Stores a collection of the file objects within directory
+    public void loadContents() {
+        foldercontents = getUZFilePath().listFiles(); 
+    }
 
+    @Override
+    public Iterator createIterator() {
+        return new FolderIterator();
+    }
+
+    // Nested class
+    private class FolderIterator implements Iterator {
+        private int i=0;
+
+        @Override
+        public boolean hasNext()
+        {
+            if (i < foldercontents.length)
+                return true;
+            
+            return false;
+        }
+
+        @Override
+        public Object next()
+        {
+            if (this.hasNext())
+                return foldercontents[i++];
+            
+            return null;
+        }  
+    }
+}
