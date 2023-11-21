91735b5187fa5ffa9e4c4812ca7ebcc724939b14
vanessa <98173531+vnsa-abn@users.noreply.github.com>
2023-11-21 20:29:00 +0000

    tests

--- src/test/java/comp3607project/NameConventionTest.java
+++ src/test/java/comp3607project/NameConventionTest.java
@@ -11,18 +11,16 @@ public class NameConventionTest {
     @Test
     public void testStudentSubmissionName()  throws IOException{
         // Define the naming convention pattern ie. 8160123456_LaraCroft_A3
-        final String studentSubName = "^[816000000-816099999_]+_[A-Za-z_]+_[A0-9_]+$";//"[816000000-816099999]_[A-Za-z]_[A][1-5]";
-
-        // Compile the pattern into a regex Pattern object
-        Pattern regexPattern = Pattern.compile(studentSubName);
+        final String studentSubName = "^[816000000-816099999_]+_[A-Za-z_]+_A[0-9_].zip";
 
         // Unzipped class folder
-        String destDirectory = "src/resources/SubTest";
+        String destDirectory = "src/resources/ClassFiles";
         String[] studentFolders = new File(destDirectory).list();
 
         // Check the name of each folder
         for (String name : studentFolders)  {
-            assertTrue(name, regexPattern.matcher(name).matches());
+            assertTrue(name.matches(studentSubName));
+            //assertTrue(name, regexPattern.matcher(name).matches());
         }
     }
 }
